6c15981ff6c862a914214f88ea095cc263263ce8
andreywilmsen <andreywilmsendepaula@gmail.com>
2026-01-30 20:12:31 -0300

    inicialização de projeto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+@extends('adminlte::page')
+
+{{-- Customize layout sections --}}
+
+@section('subtitle', 'Welcome')
+@section('content_header_title', 'Home')
+@section('content_header_subtitle', 'Welcome')
+
+{{-- Content body: main page content --}}
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+           <div class="col-12">
+            <div class="btn  my-3 text-info card">Dashboard interno</div>
+           </div>
+        </div>
+    </div>
+@stop
+
+{{-- Push extra CSS --}}
+
+@push('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@endpush
+
+{{-- Push extra scripts --}}
+
+@push('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@endpush

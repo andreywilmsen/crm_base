73d446e882ea51ba252270c1d1725685f09b7871
andreywilmsen <andreywilmsendepaula@gmail.com>
2026-02-05 21:55:52 -0300

    feat: insere arquivos de testes features.

---
+++ Modules/Core/Tests/Features/User/UserGetTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Modules\Core\Tests\Features\User;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+
+class UserGetTest extends TestCase{
+    
+    use RefreshDatabase;
+
+    #[Test]
+    public function it_should_get_user(){
+        
+    }
+}
---
+++ Modules/Core/Tests/Features/User/UserRegistrationTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Modules\Core\Tests\Features\User;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+
+class UserRegistrationTest extends TestCase{
+    
+    use RefreshDatabase;
+
+    #[Test]
+    public function it_should_register_user(){
+        
+    }
+}

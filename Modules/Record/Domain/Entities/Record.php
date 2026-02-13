33513f68c2d87a16cc5404fc0e4d3c7df68f9dca
andreywilmsen <andreywilmsendepaula@gmail.com>
2026-02-12 23:45:27 -0300

    feat: cria entidade record juntamente com getters e validadores.

--- Modules/Record/Domain/Entities/Record.php
+++ Modules/Record/Domain/Entities/Record.php
@@ -2,6 +2,80 @@
 
 namespace Modules\Record\Domain\Entities;
 
-class Record {
-    
+use InvalidArgumentException;
+
+
+class Record
+{
+
+    public function __construct(
+        private readonly string $title,
+        private readonly string $referenceDate,
+        private readonly float $value,
+        private readonly string $description,
+        private readonly string $status,
+        private readonly int $userId
+    ) {
+        $this->validate();
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getReferenceDate()
+    {
+        return $this->referenceDate;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+
+    // Validadores
+
+    private function validate()
+    {
+        if (empty(trim($this->title))) {
+            throw new InvalidArgumentException('Campo título é obrigatório.');
+        }
+
+        if (empty($this->referenceDate)) {
+            throw new InvalidArgumentException('Campo data é obrigatório.');
+        }
+
+        if (empty(trim($this->description))) {
+            throw new InvalidArgumentException('Campo descrição é obrigatório.');
+        }
+
+        if ($this->value < 0) {
+            throw new InvalidArgumentException('Valores negativos não são permitidos.');
+        }
+
+        if (empty($this->status)) {
+            throw new InvalidArgumentException('Campo status é obrigatório.');
+        }
+
+        if (empty($this->userId)) {
+            throw new InvalidArgumentException('Campo responsável é obrigatório.');
+        }
+    }
 }
--- Modules/Record/Tests/Unit/RecordEntityTest.php
+++ Modules/Record/Tests/Unit/RecordEntityTest.php
@@ -14,19 +14,114 @@ class RecordEntityTest extends TestCase
     #[Test]
     public function it_should_instance_record()
     {
-        $data = [
-            'title' => 'Orçamento de energia',
-            'data' => '2025-12-25',
-            'value' => 406.3,
-            'description' => 'Fatura de Janeiro',
-            'status' => 'pending',
-        ];
 
-        $record = new Record($data);
+        $record = new Record(
+            title: 'Orçamento de energia',
+            referenceDate: '2025-12-25',
+            value: 406.3,
+            description: 'Fatura de Janeiro',
+            status: 'pending',
+            userId: 1,
+        );
 
         $this->assertInstanceOf(Record::class, $record);
+        $this->assertEquals('Orçamento de energia', $record->getTitle());
+        $this->assertEquals(406.3, $record->getValue());
+    }
 
-        $this->assertEquals('Orçamento de energia', $record->title);
-        $this->assertEquals(406.3, $record->value);
+    #[Test]
+    public function it_should_block_with_empty_title()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Campo título é obrigatório.');
+
+        new Record(
+            title: '',
+            referenceDate: '2025-12-25',
+            value: 406.3,
+            description: 'Fatura de Janeiro',
+            status: 'pending',
+            userId: 1,
+        );
+    }
+
+    #[Test]
+    public function it_should_block_with_empty_referenceDate()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Campo data é obrigatório.');
+
+        new Record(
+            title: 'Orçamento de energia',
+            referenceDate: '',
+            value: 406.3,
+            description: 'Fatura de Janeiro',
+            status: 'pending',
+            userId: 1,
+        );
+    }
+
+    #[Test]
+    public function it_should_block_with_empty_description()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Campo descrição é obrigatório.');
+
+        new Record(
+            title: 'Orçamento de energia',
+            referenceDate: '2025-12-25',
+            value: 406.3,
+            description: '',
+            status: 'pending',
+            userId: 1,
+        );
+    }
+
+    #[Test]
+    public function it_should_block_negative_value()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Valores negativos não são permitidos.');
+
+        new Record(
+            title: 'Orçamento de energia',
+            referenceDate: '2025-12-25',
+            value: -406.3,
+            description: 'Fatura de Janeiro',
+            status: 'pending',
+            userId: 4,
+        );
+    }
+
+    #[Test]
+    public function it_should_block_with_empty_status()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Campo status é obrigatório.');
+
+        new Record(
+            title: 'Orçamento de energia',
+            referenceDate: '2025-12-25',
+            value: 406.3,
+            description: 'Fatura de Janeiro',
+            status: '',
+            userId: 1,
+        );
+    }
+
+    #[Test]
+    public function it_should_block_with_empty_userId()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Campo responsável é obrigatório.');
+
+        new Record(
+            title: 'Orçamento de energia',
+            referenceDate: '2025-12-25',
+            value: 406.3,
+            description: 'Fatura de Janeiro',
+            status: 'pending',
+            userId: 0,
+        );
     }
 }

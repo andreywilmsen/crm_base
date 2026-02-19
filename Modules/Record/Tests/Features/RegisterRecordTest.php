d902d49f28c3ce235ead346ddec4e0b703433639
andreywilmsen <andreywilmsendepaula@gmail.com>
2026-02-18 23:02:39 -0300

    feat: insere teste de feature de registro (record) para campos vazios.

--- Modules/Record/Tests/Features/RegisterRecordTest.php
+++ Modules/Record/Tests/Features/RegisterRecordTest.php
@@ -35,4 +35,99 @@ class RegisterRecordTest extends TestCase
             'user_id' => $user->id
         ]);
     }
+
+    #[Test]
+    public function it_should_block_register_with_empty_title()
+    {
+        $user = $this->loginAsFuncionario();
+
+        $data = [
+            'title'          => '',
+            'reference_date' => '2026-02-14',
+            'value'          => 2500.00,
+            'description'    => 'Serviço prestado para a empresa X.',
+            'status'         => 'completed',
+            'user_id'        => $user->id,
+        ];
+
+        $response = $this->post(route('record.store'), $data);
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['title']);
+    }
+
+    #[Test]
+    public function it_should_block_register_with_empty_description()
+    {
+        $user = $this->loginAsFuncionario();
+
+        $data = [
+            'title'          => 'Venda de Consultoria',
+            'reference_date' => '2026-02-14',
+            'value'          => 2500.00,
+            'description'    => '',
+            'status'         => 'completed',
+            'user_id'        => $user->id,
+        ];
+
+        $response = $this->post(route('record.store'), $data);
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['description']);
+    }
+
+    #[Test]
+    public function it_should_block_register_with_empty_reference_data()
+    {
+        $user = $this->loginAsFuncionario();
+
+        $data = [
+            'title'          => 'Venda de Consultoria',
+            'reference_date' => '',
+            'value'          => 2500.00,
+            'description'    => 'Serviço prestado para a empresa X.',
+            'status'         => 'completed',
+            'user_id'        => $user->id,
+        ];
+
+        $response = $this->post(route('record.store'), $data);
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['reference_date']);
+    }
+
+    #[Test]
+    public function it_should_block_register_with_empty_status()
+    {
+        $user = $this->loginAsFuncionario();
+
+        $data = [
+            'title'          => 'Venda de Consultoria',
+            'reference_date' => '2026-02-14',
+            'value'          => 2500.00,
+            'description'    => 'Serviço prestado para a empresa X.',
+            'status'         => '',
+            'user_id'        => $user->id,
+        ];
+
+        $response = $this->post(route('record.store'), $data);
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['status']);
+    }
+
+    #[Test]
+    public function it_should_block_register_with_empty_userId()
+    {
+        $this->loginAsFuncionario();
+
+        $data = [
+            'title'          => 'Venda de Consultoria',
+            'reference_date' => '2026-02-14',
+            'value'          => 2500.00,
+            'description'    => 'Serviço prestado para a empresa X.',
+            'status'         => '',
+            'user_id'        => '',
+        ];
+
+        $response = $this->post(route('record.store'), $data);
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['user_id']);
+    }
 }
